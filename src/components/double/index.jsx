841e14b3c1b93a0f66dda682abd18294267df0de
Olivier Larose <olivierlarose1@gmail.com>
2023-07-07 17:09:48 -0400

    initial commit

---
+++ src/components/double/index.jsx
@@ -0,0 +1,79 @@
+'use client';
+import styles from './style.module.scss';
+import Image from 'next/image';
+import { useRef } from 'react'; 
+
+export default function Index({projects}) {
+
+    const firstImage = useRef(null);
+    const secondImage = useRef(null);
+    let requestAnimationFrameId = null;
+    let xPercent = 0;
+    let currentXPercent = 0;
+    const speed = 0.15;
+    
+    const manageMouseMove = (e) => {
+        const { clientX } = e;
+        xPercent = (clientX / window.innerWidth) * 100;
+        
+        if(!requestAnimationFrameId){
+            requestAnimationFrameId = window.requestAnimationFrame(animate);
+        }
+    }
+
+    const animate = () => {
+        //Add easing to the animation
+        const xPercentDelta = xPercent - currentXPercent;
+        currentXPercent = currentXPercent + (xPercentDelta * speed)
+        
+        //Change width of images between 33.33% and 66.66% based on cursor
+        const firstImagePercent = 66.66 - (currentXPercent * 0.33);
+        const secondImagePercent = 33.33 + (currentXPercent * 0.33);
+        firstImage.current.style.width = `${firstImagePercent}%`
+        secondImage.current.style.width = `${secondImagePercent}%`
+        
+        if(Math.round(xPercent) == Math.round(currentXPercent)){
+            window.cancelAnimationFrame(requestAnimationFrameId);
+            requestAnimationFrameId = null;
+        }
+        else{
+            window.requestAnimationFrame(animate)
+        }
+    }
+
+    return(
+      <div onMouseMove={(e) => {manageMouseMove(e)}} className={styles.double}>
+  
+        <div ref={firstImage} className={styles.imageContainer}>
+          <div className={styles.stretchyWrapper}>
+            <Image 
+              src={`/images/${projects[0].src}`}
+              fill={true}
+              alt={"image"}
+            />
+          </div>
+          <div className={styles.body}>
+              <h3>{projects[0].name}</h3>
+              <p>{projects[0].description}</p>
+              <p>{projects[0].year}</p>
+          </div>
+        </div>
+  
+        <div ref={secondImage} className={styles.imageContainer}>
+          <div className={styles.stretchyWrapper}>
+            <Image 
+              src={`/images/${projects[1].src}`}
+              fill={true}
+              alt={"image"}
+            />
+          </div>
+          <div className={styles.body}>
+              <h3>{projects[1].name}</h3>
+              <p>{projects[1].description}</p>
+              <p>{projects[1].year}</p>
+          </div>
+        </div>
+  
+      </div>
+    )
+  }
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+    {
+        name: "Google Pixel Creator Labs",
+        client: "Grow",
+        description: "A partnership between Google & SN37.",
+        src: "google.jpg",
+        year: 2023,
+    },
+    {
+        name: "Decimal",
+        client: "Decimal",
+        description: "Portfolio site for Decimal.",
+        src: "decimal.jpg",
+        year: 2023,
+    },
+    {
+        name: "MAVEN 11",
+        client: "analogueagency",
+        description: "New web design for the blockchain investment fund Maven 11.",
+        src: "maven.jpg",
+        year: 2022,
+    },
+    {
+        name: "Wix Playground Homepage",
+        client: "Wix Playground",
+        description: "Wix Playground is powered by the Wix.com design team.",
+        src: "wix.jpg",
+        year: 2022,
+    },
+    {
+        name: "POWELL—STUDIO",
+        client: "POWELL—STUDIO",
+        description: "The online presence for Powell—Studio.",
+        src: "powell.jpg",
+        year: 2023,
+    },
+    {
+        name: "ROCKETPANDA",
+        client: "ET Studio",
+        description: "An Italian animation studio.",
+        src: "panda.jpg",
+        year: 2022,
+    },
+    {
+        name: "C2 Montreal",
+        client: "Ingamana",
+        description: "C2 Montréal is Canada's premier creative-business event.",
+        src: "c2.jpg",
+        year: 2021,
+    },
+    {
+        name: "Design Is Funny",
+        client: "Shader Studio",
+        description: "Funny is the design work of Daniele Buffa.",
+        src: "funny.jpg",
+        year: 2020,
+    }
+]
+
